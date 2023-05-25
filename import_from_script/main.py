21b350b1cdccbbb08675c7913cf063ab1ba1f9d5
Adrienne Anderson <adrienne.j.anderson@gmail.com>
2023-05-25 12:15:46 -0500

    create import from scripts example

---
+++ import_from_script/main.py
@@ -0,0 +1,8 @@
+from script1 import add_numbers
+from script2 import subtract_numbers
+
+a = 5
+b = 7
+
+print(add_numbers(a,b))
+print(subtract_numbers(a,b))
---
+++ import_from_script/script2.py
@@ -0,0 +1,2 @@
+def subtract_numbers(x: int, y: int) -> int:
+    return x - y
